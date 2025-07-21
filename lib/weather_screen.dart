bc72387391634a237440cf54d6ea2fc96d56dcd2
Muhammad Hassan <mhassan.codes@gmail.com>
2025-07-21 15:48:07 -0700

    creating layout structure

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -14,6 +14,17 @@ class WeatherScreeen extends StatelessWidget {
         centerTitle: true,
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
       ),
+      body: Column(
+        children: [
+          // main card
+          const Placeholder(fallbackHeight: 250),
+          const SizedBox(height: 25),
+          // Weather forecast cards
+          const Placeholder(fallbackHeight: 150),
+          const SizedBox(height: 20),
+          const Placeholder(fallbackHeight: 150),
+        ],
+      ),
     );
   }
 }
