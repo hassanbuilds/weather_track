174618339170434d9be48cde7fb3a02350e9765f
Muhammad Hassan <mhassan.codes@gmail.com>
2025-07-21 14:29:16 -0700

    starting the project from the scratch

---
+++ lib/weather_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreeen extends StatelessWidget {
+  const WeatherScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
